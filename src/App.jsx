b8bce2aa8b0ebd9ec5d21eabbef7b0e9b56614d4
jakir-git <carrier.jakir@gmail.comm>
2024-01-14 02:21:52 +0600

    Passing props from Paren to child component

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 /*
   Game
     -> Board
@@ -5,26 +6,36 @@
     -> History
 */
 
-function Square({ value }) {
+function Square({ value, onSquareClick }) {
   return (
-    <button className="bg-white border border-gray-400 h-12 w-12 leading-9 text-lg">
+    <button
+      onClick={onSquareClick}
+      className="bg-white border border-gray-400 h-12 w-12 leading-9 text-lg"
+    >
       {value}
     </button>
   );
 }
 
 export default function Board() {
+  const [squares, setSquares] = useState(Array(8).fill(null));
+
+  function handleClick() {
+    squares[0] = "X";
+    setSquares([...squares]);
+  }
+
   return (
     <div className="grid gap-2 grid-cols-3 grid-rows-3 w-fit">
-      <Square value="1" />
-      <Square value="2" />
-      <Square value="3" />
-      <Square value="4" />
-      <Square value="5" />
-      <Square value="6" />
-      <Square value="7" />
-      <Square value="8" />
-      <Square value="9" />
+      <Square value={squares[0]} onSquareClick={handleClick} />
+      <Square value={squares[1]} onSquareClick={handleClick} />
+      <Square value={squares[2]} onSquareClick={handleClick} />
+      <Square value={squares[3]} onSquareClick={handleClick} />
+      <Square value={squares[4]} onSquareClick={handleClick} />
+      <Square value={squares[5]} onSquareClick={handleClick} />
+      <Square value={squares[6]} onSquareClick={handleClick} />
+      <Square value={squares[7]} onSquareClick={handleClick} />
+      <Square value={squares[8]} onSquareClick={handleClick} />
     </div>
   );
 }
