365a8b46ae1935a5b4086a28f11c6d83f0caa317
jakir-git <carrier.jakir@gmail.comm>
2024-01-14 01:33:08 +0600

    Created game board layout

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,26 @@
     -> History
 */
 
-export default function Square() {
-  return <h1 className="text-3xl font-bold underline">Tic Tac Toe</h1>;
+function Square({ value }) {
+  return (
+    <button className="bg-white border border-gray-400 h-12 w-12 leading-9 text-lg">
+      {value}
+    </button>
+  );
+}
+
+export default function Board() {
+  return (
+    <div className="grid gap-2 grid-cols-3 grid-rows-3 w-fit">
+      <Square value="1" />
+      <Square value="2" />
+      <Square value="3" />
+      <Square value="4" />
+      <Square value="5" />
+      <Square value="6" />
+      <Square value="7" />
+      <Square value="8" />
+      <Square value="9" />
+    </div>
+  );
 }
